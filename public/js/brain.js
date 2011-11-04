260d109eaec7b3e145ef3f395faaf2d031bbfc76
Nic Luciano <caden@catshirt.net>
2011-11-04 05:12:01 -0400

    adding scrollview

--- public/js/brain.js
+++ public/js/brain.js
@@ -29,5 +29,33 @@
 	(new Repos()).fetch({ success: function(repos) {
 		console.log(_.sprintf('%s\'s repos', username), repos);
 	}});
+
+	// scrollview
+
+	(function() {
+
+		var $win = $(window),
+			$header = $(':jqmData(role="header")'),
+			$views = $(':jqmData(role="page")');
+		
+		function resize_scrollview($view) {
+			$view.height($win.height() - ($header.outerHeight() || 0));
+		}
+
+		$views.each(function() {
+			
+			var $view = $(this);
+
+			$view.scrollview({ direction: 'y' });
+
+			resize_scrollview($view);
+
+			$win.bind('orientationchange', function() {
+				resize_scrollview($view);	
+			});
+
+		});
+
+	})();
 	
 })();
