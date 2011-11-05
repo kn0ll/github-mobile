7d601a62080ce1c6796cbf01a7d0b26237846557
Nic Luciano <caden@catshirt.net>
2011-11-05 01:32:34 -0400

    replacing jqm with iscroll

--- public/js/scrollview.js
+++ public/js/scrollview.js
@@ -1,24 +1,35 @@
-$(function() {
-	
-	if (!$.support.touch) {
-		return false;
+;var GH = (function(gh) {
+
+	// only enable scrollview for touch devices
+	if (!('ontouchend' in document)) {
+		return gh;
 	}
 
-	var $win = $(window),
-		$header = $(':jqmData(role="header")'),
-		$views = $(':jqmData(role="page")');
-	
-	function resize_scrollview($view) {
-		$view.height($win.height() - ($header.outerHeight() || 0));
-	}
+	$(function() {
 
-	$views.each(function() {
-		var $view = $(this);
-		$view.scrollview({ direction: 'y' });
-		resize_scrollview($view);
-		$win.bind('orientationchange', function() {
-			resize_scrollview($view);	
+		var s = '.scrollview',
+			$doc = $(document);
+		
+		// create each scrollview
+		$(s).each(function() {
+			$(this).data({
+				scrollview: new iScroll(this)
+			});
 		});
+
+		// prevent scrolling the actual doc
+		$doc.bind('touchmove', function(e) {
+			e.preventDefault();
+		});
+
+		// trigger 'modified' from within scrollviews
+		// to notify scrollview for refresh
+		$doc.bind('modified', function(e) {
+			$(e.target).closest(s).data('scrollview').refresh();
+		});
+
 	});
-	
-});
+
+	return gh;
+
+})(window['GH'] || {});
--- public/js/views.js
+++ public/js/views.js
@@ -4,6 +4,32 @@
 		
 	};
 
+	Views.prototype.Nav = Backbone.View.extend({
+		events: {
+			'click a': 'select'	
+		},
+		initialize: function() {
+			var self = this;
+			_.bindAll(this);
+			this.model = new Backbone.Model({
+				$selected: $('.selected', self.el)
+			});
+			this.model.bind('change:$selected', this.render);
+		},
+		select: function(e) {
+			this.model.set({
+				$selected: $(e.target)
+			});
+		},
+		render: function(model, $selected) {
+			var $prev = model.previous('$selected');
+			if ($prev) {
+				$prev.removeClass('selected');
+			}
+			$selected.addClass('selected');	
+		}
+	});
+
 	Views.prototype.News = Backbone.View.extend({
 		initialize: function() {
 			_.bindAll(this);
@@ -14,6 +40,7 @@
 			var self = this;
 			$.get('/jst/events.jst', function(tmp) {
 				self.el.empty().append(_.template(tmp, self));
+				self.el.trigger('modified');
 			});
 		}
 	});
