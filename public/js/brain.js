1bf54c01173f445c09450e323bd7b796558a3430
Nic Luciano <caden@catshirt.net>
2011-11-04 20:23:02 -0400

    adding rudimentary view for news

--- public/js/brain.js
+++ public/js/brain.js
@@ -1,34 +1,57 @@
-;(function() {
+$(function() {
 
 	var username = GH.User.get('login');
+
+	// give collections jsonp functionality
+
+	Backbone.Collection = Backbone.Collection.extend({
+		sync: function(method, model, options) {
+			options.dataType = 'jsonp';
+			options.url = _.sprintf('https://api.github.com%s', this.url);
+			return Backbone.sync(method, model, options);
+		},
+		parse: function(resp, xhr) {
+			return resp.data;
+		}
+	});
 
 	// collections
 
 	var News = Backbone.Collection.extend({
-		url: _.sprintf('/v3/users/%s/received_events/public', username)
+		url: _.sprintf('/users/%s/received_events/public', username)
 	});
 
 	var Events = Backbone.Collection.extend({
-		url: _.sprintf('/v3/users/%s/events', username)
+		url: _.sprintf('/users/%s/events', username)
 	});
 
 	var Repos = Backbone.Collection.extend({
-		url: _.sprintf('/v3/users/%s/repos', username)
+		url: _.sprintf('/users/%s/repos', username)
 	});
 
 	var Watched = Backbone.Collection.extend({
-		url: _.sprintf('/v3/users/%s/watched', username)
+		url: _.sprintf('/users/%s/watched', username)
 	});
 
-	// initiate collections
+	// views
+	
+	var NewsView = Backbone.View.extend({
+		initialize: function() {
+			_.bindAll(this);
+			this.collection = new News();
+			this.collection.fetch({ success: this.render });
+		},
+		render: function() {
+			var self = this;
+			$.get('/jst/events.jst', function(tmp) {
+				self.el.empty().append(_.template(tmp, self));
+			});
+		}
+	});
 
-	(new News()).fetch({ success: function(news) {
-		console.log(_.sprintf('%s\'s news', username), news);
-	}});
-
-	(new Repos()).fetch({ success: function(repos) {
-		console.log(_.sprintf('%s\'s repos', username), repos);
-	}});
+	new NewsView({
+		el: $(':jqmData(role="content")', $('#news'))
+	});
 
 	// scrollview
 
@@ -58,4 +81,4 @@
 
 	})();
 	
-})();
+});
