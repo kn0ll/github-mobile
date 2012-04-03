9ebf72611b3f5930d69a3cff81557cf3135ffa3c
nic luciano <caden@catshirt.net>
2012-04-03 15:23:06 -0700

    thigns

--- public/js/brain.js
+++ public/js/brain.js
@@ -2,7 +2,7 @@
   var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
   $(function() {
-    var Nav, Router, manifest, pages, router;
+    var Nav, Router, pages, router;
     pages = $('#content').pages();
     Nav = new GH.Views.Nav({
       el: $('#nav')
@@ -54,9 +54,10 @@
     })();
     router = new Router;
     Backbone.history.start();
-    manifest = 'http://gh.nodejitsu.com/manifest.json';
-    if (confirm('Wow! You\'re totally on B2G!! Install this one your home screen?')) {
-      alert('Thanks!!');
+    if (navigator.mozApps) {
+      if (confirm('Wow! You\'re totally on B2G!! Install this one your home screen?')) {
+        navigator.mozApps.install('http://gh.nodejitsu.com/manifest.json');
+      }
     }
     return $(function() {
       return $('a').live('click', function(e) {
